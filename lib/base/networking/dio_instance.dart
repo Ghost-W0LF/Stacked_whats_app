07a2ed9f2742c3886762a31f95aadd3763907073
Ghost-W0LF <Abhinab1221@gmail.com>
2024-09-16 10:48:36 +0545

    chore: minor changes

--- lib/base/networking/dio_instance.dart
+++ lib/base/networking/dio_instance.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:stacked_app/base/networking/interceptor/auth_interceptor.dart';
 
 class DioInstance {
   DioInstance() {
     // Add AuthInterceptor to Dio
-    //dio.interceptors.add(AuthInterceptor());
-    //dio.interceptors.add(LogInterceptor());
+    dio.interceptors.add(AuthInterceptor());
+    dio.interceptors.add(LogInterceptor());
   }
   final dio = Dio(BaseOptions());
 }
--- lib/ui/views/signup/signup_viewmodel.dart
+++ lib/ui/views/signup/signup_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_app/app/app.locator.dart';
+import 'package:stacked_app/app/app.router.dart';
 import 'package:stacked_app/services/cloud_firestore_service.dart';
 import 'package:stacked_app/services/firebase_auth_service.dart';
 import 'package:stacked_app/ui/views/signup/signup_repository/signup_repository_implements.dart';
@@ -9,6 +10,7 @@ import 'package:stacked_services/stacked_services.dart';
 
 class SignupViewModel extends FormViewModel with $SignupForm {
   final snackBar = locator<SnackbarService>();
+  final navigation = locator<NavigationService>();
   final register = SignupRepositoryImplements();
   final registerFirebase = locator<FirebaseAuthService>();
   final cloudFireStore = locator<CloudFirestoreService>();
@@ -19,6 +21,7 @@ class SignupViewModel extends FormViewModel with $SignupForm {
       if (message == 'success') {
         cloudFireStore.addData(fullnameController.text, emailController.text,
             passwordController.text);
+        navigation.replaceWithHomeViewView();
         snackBar.showSnackbar(
             message: "Account created",
             duration: const Duration(milliseconds: 500));
